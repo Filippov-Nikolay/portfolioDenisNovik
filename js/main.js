0b26d4691a17945e629ae2110e6a8edad1e780bb
Filippov-Nikolay <filippovnikolay195@gmail.com>
2022-07-09 22:32:52 +0300

    main

--- js/main.js
+++ js/main.js
@@ -6,3 +6,27 @@ function openbox(box) {
         document.getElementById(box).style.display='none';
     }
 }
+
+let linkList = document.querySelectorAll('.menu__link');
+linkList.forEach(link => {
+    link.addEventListener('click', function(e) {
+        linkList.forEach(link => {
+            link.classList.remove('menu__link--active');
+        });
+        linkList.forEach(link => {
+            this.classList.add('menu__link--active');
+        });
+    });
+});
+
+let langList = document.querySelectorAll('.language__item');
+langList.forEach(lang => {
+    lang.addEventListener('click', function(e) {
+        langList.forEach(lang => {
+            lang.classList.remove('language__item--active');
+        });
+        langList.forEach(lang => {
+            this.classList.add('language__item--active');
+        });
+    });
+});
